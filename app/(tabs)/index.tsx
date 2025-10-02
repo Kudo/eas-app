b8edf4e241cd03b96627d6e045b323b2d8bee73f
Kudo Chien <kudo@expo.dev>
2025-10-02 20:40:46 +0800

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One123456</Text>
+      <Text style={styles.title}>Tab One123 with maps</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Image source={{ uri: 'https://picsum.photos/id/238/800/800' }} />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
